37dde05b713b67fdfccc3ec2b4455ef92ed5e1dd
Tatum <tmaston@ucsd.edu>
2020-01-23 10:43:10 -0800

    commit

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,22 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Hello World");
+		$(".jumbotron p").toggleClass("active");
+	$("a.thumbnail").click(projectClick);
 	});
 
+function projectClick(e) {
+	// prevent the page from reloading 
+    e.preventDefault();
+		var containingProject = $(this).closest(".project");
+		var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(".project-description").fadeOut();
+    }
+}
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
